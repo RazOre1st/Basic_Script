e3a43ce1aa9aca58bf19a0fbca8469281cbbe85f
RazOre1st <88528720+RazOre1st@users.noreply.github.com>
2021-08-20 17:46:45 +0700

    Add files via upload

---
+++ Latihan4.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan4 : MonoBehaviour {
+
+	Karakter player1;
+	void Start () {
+		player1 = new Karakter ();
+		player1.Name = "Razor";
+		player1.Health = 200;
+		player1.Damage = 50;
+		player1.Jump();
+	}
+	
+		void Update () 
+	{
+		
+	}
+}
